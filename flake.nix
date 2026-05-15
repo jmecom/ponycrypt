b11394d573241393fcd62c60b17358f6aa907dcb
Jordan Mecom <jm@squareup.com>
2026-05-15 12:25:13 -0700

    Add Nix flake for Pony

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Pony language development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.ponyc;
+          ponyc = pkgs.ponyc;
+          corral = pkgs.pony-corral;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              ponyc
+              pony-corral
+            ];
+
+            shellHook = ''
+              echo "Pony $(ponyc --version | head -n 1)"
+            '';
+          };
+        });
+    };
+}
